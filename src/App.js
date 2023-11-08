672b7e8bc33b7707c9ce5afcb6d188ecb107a3f9
dave <shijildave@gmail.com>
2023-11-08 22:42:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,3 @@
-export function App() {
+export default function App() {
   return <div>Hello world</div>;
 }
--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ const container = document.getElementById("app");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <></>
+    <App />
   </React.StrictMode>
 );
